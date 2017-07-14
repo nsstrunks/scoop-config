5539e5b13db356b742ca397c617317b5ac934721
nsstrunks <nsstrunks@gmail.com>
2017-07-13 21:19:37 -0400

    Install script for scoop tools

---
+++ install.ps1
@@ -0,0 +1,16 @@
+# Add extras repo
+scoop install 7zip git-with-openssh
+scoop bucket add extras
+
+#Theming Powershell to look "prettier"
+scoop install concfg pshazz
+concfg export console-backup.json
+confg import solarized.json small
+
+pshazz config editor subl
+pshazz get https://raw...github
+pshazz use nessy
+
+
+#Basic tools
+scoop install sublime-text oraclejdk cloudfoundry-cli idea sqlitebrowser tomcat busybox curl cacert maven nodejs yarn
